a56b0c74c53c00bc19207e7546aa78fcdb49e7e5
Justin Ramirez <ramirezbjustin@gmail.com>
2020-09-07 23:08:44 -0400

    adding in mongo database cluster in server.js

--- server.js
+++ server.js
@@ -16,10 +16,14 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/books", {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://justin_ramirezzz:<naruto#071995>@cluster0.wqlsj.mongodb.net/<books>?retryWrites=true&w=majority",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  }
+);
 
 // Start the API server
 app.listen(PORT, () =>
